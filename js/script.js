8e753eccb9c435936cf3e491f4a011f44fa8e6eb
Maxim <redko.mak@yandex.ru>
2023-01-11 20:45:28 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/* let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number); */
+
+
+let userName = 'John';
+
+
+let userNumber = 25;
+
+
+userNumber = 24;
+
+
+
+let storeName = 'Dota 2';
+
+
+let storeDescription = {
+    budget: 10000,
+    employees: ['Gaben', 'Gazar' , 'Mamon'],
+    products: { 
+        hook: 200,
+        arcana: 300 
+    },
+    open: true 
+};
