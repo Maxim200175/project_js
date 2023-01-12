15ae0870cc16e61d45a3568bca1ee6c2a475df02
Maxim <redko.mak@yandex.ru>
2023-01-12 17:58:39 +0300

    films

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ number = 10;
 console.log(number); */
 
 
-let userName = 'John';
+/* let userName = 'John';
 
 
 let userNumber = 25;
@@ -28,4 +28,28 @@ let storeDescription = {
         arcana: 300 
     },
     open: true 
+}; */
+
+const numberOfFilms = prompt('Сколько фильмов вы смотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false 
 };
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('Насколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('Насколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
