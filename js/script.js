1605c333aa0eb14f0144d647254edea7271e7ce5
Maxim <redko.mak@yandex.ru>
2023-01-21 19:46:24 +0300

    films cyrcle

--- js/script.js
+++ js/script.js
@@ -44,12 +44,78 @@ const personalMovieDB = {
     privat: false 
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('Насколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('Насколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('Насколько оцените его?', '');
+
+    if (a !=null && b !=null && a !='' && b !='' && a.length <50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+    
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено очень мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >=30 ) {
+    console.log("Вы киноман");
+} else {
+    console.log("произошла ошибка!");
+}
 
 console.log(personalMovieDB);
+
+/* if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+
+let num = 50; 
+while(num <= 55) {
+    console.log(num);
+    num++;
+}
+
+let result = '';
+const length = 7;
+
+for( let i = 1; i < length; i++) {
+    for (let j = 0; j<i; j++) {
+        result += '*';
+    }
+    result += '\n';
+}
+console.log(result);
+
+let cycle = 5;
+while(cycle <=10) {
+    console.log(cycle);
+    cycle++;
+}
+
+
+for( let i = 20; i >= 10; i--) {
+  if (i === 13) break;
+  console.log(i);
+}
+
+for( i = 2; i<=10; i++) {
+    if(i % 2 === 0) 
+    console.log(i);
+}
+
+let Numb = 2;
+while(Numb <= 16) {
+    if(Numb % 0 === 2); 
+    Numb++;
+    console.log(Numb);
+} */
